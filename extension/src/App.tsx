90ced7fc0ba73e029f3f79404a454d85326c30b3
Sajjan Kumar <sajjanaasht@gmail.com>
2026-02-18 23:27:47 +0530

    update popup

--- extension/src/App.tsx
+++ extension/src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div>
-      <h1 className="bg-amber-700 text-7xl text-white">Welcome to React with TypeScript!</h1>
+      <h1 className="bg-amber-700 text-7xl text-white">Welcome to extension , make your notes easy!</h1>
     </div>
   )
 }
