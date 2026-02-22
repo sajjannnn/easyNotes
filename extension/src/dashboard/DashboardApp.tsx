0119a6b6b5e6360ca191694445c57fe3594e2c5b
Sajjan Kumar <sajjanaasht@gmail.com>
2026-02-22 22:51:32 +0530

    dashboard change

--- extension/src/dashboard/DashboardApp.tsx
+++ extension/src/dashboard/DashboardApp.tsx
@@ -3,7 +3,7 @@ const DashboardApp = () => {
   return (
     <div style={{ padding: "20px" }}>
       <h1>Easy Notes Dashboard</h1>
-      <p>This is where folders and files will appear.</p>
+      <p>This is where folders and files will appear</p>
     </div>
   );
 };
